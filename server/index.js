3d178ea977e095777307f140051087e9ce8ecdb7
Максим Зимовец <maksimzimovec@MacBook-Air-Maksim.local>
2024-05-24 11:32:17 +0300

    server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ import Datastore from "nedb";
 export const database = new Datastore("database.db");
 database.loadDatabase();
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(__dirname + "/build"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/build" + "/index.html");
+});
+
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 
