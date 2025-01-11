1532c5903ed383e2f29ccea0273753a55c5d2904
Victor Popov <popov.v@volgablob.ru>
2025-01-11 14:11:28 +0300

    Чероновик загрузки файла расписания

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import bodyParser from "body-parser";
+import docxParser from "docx-parser";
 
 //--------NeDB---------
 import Datastore from "nedb";
@@ -14,20 +15,19 @@ import mammoth from "mammoth";
 
 import multer from "multer";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(__dirname + "/build"));
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/uploads");
+    cb(null, __dirname + "/uploads");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage: storage });
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.use(express.static(__dirname + "/build"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/build" + "/index.html");
@@ -69,22 +69,37 @@ app.post("/schedule", urlencodedParser, (req, res) => {
 });
 
 app.post("/upload", upload.single("docx"), function (req, res, next) {
+  console.log("POST /upload", req.file);
   const fileName = req.file.originalname;
-  setTimeout(() => {
-    mammoth
-      .convertToHtml({
-        path: __dirname + `/uploads/${fileName}`,
-      })
-      .then(function (result) {
-        var html = result.value; // The generated HTML
-        var messages = result.messages; // Any messages, such as warnings during conversion
-        res.send(html);
-        console.log("html", html);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, 2000);
+
+  docxParser.parseDocx(__dirname + `/uploads/${fileName}`, function (data) {
+    // console.log(data);
+
+    const trimmed = data.replace(
+      "Расписание Богослужений\nв Александр-Невском кафедральном соборе\nДень недели, устав\nСвятые дня",
+      ""
+    );
+    const ar = trimmed.split(/\d\s.+г\./);
+    console.log("trimmed", trimmed, ar);
+    res.json({ text: trimmed });
+  });
+
+  // setTimeout(() => {
+  // mammoth
+  //   .convertToHtml({
+  //     path: __dirname + `/uploads/${fileName}`,
+  //   })
+  //   .then(function (result) {
+  //     var html = result.value; // The generated HTML
+  //     var messages = result.messages; // Any messages, such as warnings during conversion
+  //     res.send(html);
+  //     console.log("html", html);
+  //     console.log("messages", messages);
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
+  // }, 2000);
 });
 
 app.use("/uploads", express.static("uploads"));
